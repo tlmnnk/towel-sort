b76c752beb91c82d10d5a353c5935bebf2191116
vladislav t <vt118@mail.ru>
2020-02-21 21:47:37 +0300

    feat: finish function implementation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ module.exports = function towelSort (matrix) {
 
   if(!matrix) { return []; }
   
-  return [];
-
+  return matrix.reduce((acc, item, i) => {
+    acc = [...acc, ...(i % 2 === 0 ? item : item.reverse())];
+    return acc;
+  }, []);
 };
